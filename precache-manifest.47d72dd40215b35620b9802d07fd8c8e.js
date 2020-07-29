72c6abc27b5e63bb51ca072d86ceb661c0578020
yaroslavoz <yaroslavozv@gmail.com>
2020-07-29 13:12:35 +0300

    Updates

---
+++ precache-manifest.47d72dd40215b35620b9802d07fd8c8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298dd5bd182f9653a323f81993188a2b",
+    "url": "/starwars_test/index.html"
+  },
+  {
+    "revision": "1d08172fcb76d4f744e2",
+    "url": "/starwars_test/static/css/main.54b76ceb.chunk.css"
+  },
+  {
+    "revision": "511d1333c2f94c0090e0",
+    "url": "/starwars_test/static/js/2.68adaf7c.chunk.js"
+  },
+  {
+    "revision": "1d08172fcb76d4f744e2",
+    "url": "/starwars_test/static/js/main.55bc2def.chunk.js"
+  },
+  {
+    "revision": "535fbcc746bfe80c6067",
+    "url": "/starwars_test/static/js/runtime~main.f2d47be9.js"
+  },
+  {
+    "revision": "13a5abbf6bcbc34187e54b8979c2c704",
+    "url": "/starwars_test/static/media/poster.13a5abbf.jpg"
+  },
+  {
+    "revision": "e29d1e5452943111b78a25f6e2dc8e3c",
+    "url": "/starwars_test/static/media/wall.e29d1e54.jpg"
+  }
+]);
